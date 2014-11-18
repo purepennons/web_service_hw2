90af014fa1112e124cedac0e6b8ca779c917f2ed
purepennons <y80811@gmail.com>
2014-11-19 01:41:36 +0800

    Let submit button can redirect to the result page.

--- website/public/javascripts/function.js
+++ website/public/javascripts/function.js
@@ -1,5 +1,9 @@
 
 function isbnSubmit() {
 	var isbn = $('#inputISBN').val();
-	window.location.href = location.href + '/result/' + isbn;
+	// console.log(location.href.charAt(location.href.length-1) == '/');
+	var redirectURL = 'http://' + location.host + '/restService/result/' + isbn;
+	console.log(redirectURL);
+	// var redirectURL = (location.href.charAt(location.href.length - 1) == '/')? location.href + 'result/' + isbn : location.href + '/result/' + isbn;
+	window.location.href = redirectURL;
 }
